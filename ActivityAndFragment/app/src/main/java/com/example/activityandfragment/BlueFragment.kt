d0fd467cd2679ef3c817ba0d1e92066dea878407
DESKTOP-GNOW\gnow <gnow@chungbuk.ac.kr>
2023-01-31 19:07:05 +0900

    Add annotation

--- ActivityAndFragment/app/src/main/java/com/example/activityandfragment/BlueFragment.kt
+++ ActivityAndFragment/app/src/main/java/com/example/activityandfragment/BlueFragment.kt
@@ -7,10 +7,10 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class BlueFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        saveInstanceState: Bundle?
+    override fun onCreateView(      // Fragment 의 layout 을 연결할 때 쓰는 callback
+        inflater: LayoutInflater,   // 뷰를 생성하는 객체
+        container: ViewGroup?,      // 생성할 뷰(자식 뷰)가 들어갈 부모 뷰
+        saveInstanceState: Bundle?  // 이전 Fragment 객체에서 전달된 데이터(Bundle)
     ): View? {
         return inflater.inflate(R.layout.fragment_blue, container, false);
     }
--- ActivityAndFragment/app/src/main/java/com/example/activityandfragment/TwoColorActivity.kt
+++ ActivityAndFragment/app/src/main/java/com/example/activityandfragment/TwoColorActivity.kt
@@ -12,19 +12,21 @@ class TwoColorActivity : AppCompatActivity() {
         settingButtons()
     }
 
-    fun settingButtons() {
-        val redButton = findViewById<Button>(R.id.button_red_fragment)
-        val blueButton = findViewById<Button>(R.id.button_blue_fragment)
+    fun settingButtons() {  // 뷰가 생성되었을 때 실행됨 → 빨강과 파랑 버튼이 클릭 되었을 때 행동 정의
+        val redButton = findViewById<Button>(R.id.button_red_fragment)      // 빨간 버튼 초기화
+        val blueButton = findViewById<Button>(R.id.button_blue_fragment)    // 파란 버튼 초기화
 
         redButton.setOnClickListener {
-            val fragmentTransaction = supportFragmentManager.beginTransaction()
-            fragmentTransaction.replace(R.id.fragmentFrame, RedFragment())
-            fragmentTransaction.commit()
+            val fragmentTransaction =   // FragmentTransaction 클래스 객체 생성
+                supportFragmentManager.beginTransaction()   // Fragment 추가, 삭제, 교체 등 가능
+            fragmentTransaction.replace(R.id.fragmentFrame, RedFragment())  // replace = 교체
+            fragmentTransaction.commit()    // Transaction 이후 반드시 commit() 함수 호출
         }
         blueButton.setOnClickListener {
-            val fragmentTransaction = supportFragmentManager.beginTransaction()
-            fragmentTransaction.replace(R.id.fragmentFrame, BlueFragment())
-            fragmentTransaction.commit()
+            val fragmentTransaction =   // FragmentTransaction 클래스 객체 생성
+                supportFragmentManager.beginTransaction()   // Fragment 추가, 삭제, 교체 등 가능
+            fragmentTransaction.replace(R.id.fragmentFrame, BlueFragment())  // replace = 교체
+            fragmentTransaction.commit()    // Transaction 이후 반드시 commit() 함수 호출
         }
     }
 }
