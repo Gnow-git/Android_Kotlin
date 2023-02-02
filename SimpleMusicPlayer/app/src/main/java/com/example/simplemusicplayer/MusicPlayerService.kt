e6423fd2a0a63894f859eac5d2272d3e9c2a194b
DESKTOP-GNOW\gnow <gnow@chungbuk.ac.kr>
2023-02-02 19:42:46 +0900

    mBinder, inner class

--- SimpleMusicPlayer/app/src/main/java/com/example/simplemusicplayer/MusicPlayerService.kt
+++ SimpleMusicPlayer/app/src/main/java/com/example/simplemusicplayer/MusicPlayerService.kt
@@ -2,9 +2,21 @@ package com.example.simplemusicplayer
 
 import android.app.Service
 import android.content.Intent
+import android.media.MediaPlayer
+import android.os.Binder
 import android.os.IBinder
 
 class MusicPlayerService  : Service() {
+
+    var mMediaPlayer: MediaPlayer? = null // 미디어 플레이어 객체 초기화
+
+    var mBinder: MusicPlayerBinder = MusicPlayerBinder()    // mBinder 객체 생성
+    // 내부 클래스 MusicPlayerBinder 생성, Binder 상속
+    inner class MusicPlayerBinder : Binder() {
+        fun getService(): MusicPlayerService {  // getService 함수를 가지고 있음
+            return this@MusicPlayerService  // 현재 서비스 반환 → 연결된 구성요소가 서비스의 함수 사용 가능
+        }
+    }
 
     override fun onCreate() {   // 서비스가 생성될 때 딱 한 번만 실행
         super.onCreate()
@@ -14,12 +26,13 @@ class MusicPlayerService  : Service() {
     // 구성요소에서 bindService() 함수를 호출할 때 실행, 서비스와 구성요소를 이어주는 매개체 IBinder 반환
     // 시작되고 바인드된 서비스이므로 반드시 구현, 바인드가 필요 없는 서비스는 null 반환
     override fun onBind(intent: Intent?): IBinder? {
-        TODO("NOT yet implemented")
+        return mBinder  // 위에서 생성한 mBinder 객체를 반환값으로 전달
     }
 
     // startService()나 startForegroundService() 호출할 때 실행, 시작된 상태 및 백그라운드에 존재
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        return super.onStartCommand(intent, flags, startId)
+        // 반드시 정숫값 반환, 시스템이 서비스를 중단하면 서비스 다시 실행, onStartCommand() 함수 호출
+        return START_STICKY
     }
 
     // 서비스 생명 주기의 마지막 단계, onCreate()에서 상태표시줄에 보여주었던 알림 해제
