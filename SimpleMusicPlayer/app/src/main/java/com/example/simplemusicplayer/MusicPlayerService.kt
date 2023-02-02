53b2e92184d320a66d1338fb98e14fb14d9549b7
DESKTOP-GNOW\gnow <gnow@chungbuk.ac.kr>
2023-02-02 20:17:04 +0900

    notification channel, notification, StopService

--- SimpleMusicPlayer/app/src/main/java/com/example/simplemusicplayer/MusicPlayerService.kt
+++ SimpleMusicPlayer/app/src/main/java/com/example/simplemusicplayer/MusicPlayerService.kt
@@ -1,9 +1,13 @@
 package com.example.simplemusicplayer
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.Service
 import android.content.Intent
 import android.media.MediaPlayer
 import android.os.Binder
+import android.os.Build
 import android.os.IBinder
 
 class MusicPlayerService  : Service() {
@@ -35,13 +39,43 @@ class MusicPlayerService  : Service() {
         return START_STICKY
     }
 
-    // 서비스 생명 주기의 마지막 단계, onCreate()에서 상태표시줄에 보여주었던 알림 해제
-    override fun onDestroy() {
-        super.onDestroy()
+    // 알림 채널 생성, API 26부터는 서비스 실행됨을 알림
+    fun startForegroundService() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val notificationManager = getSystemService(NOTIFICATION_SERVICE)
+                    as NotificationManager
+            val mChannel = NotificationChannel( // 알림 채널 생성
+                    "CHANNEL_ID",
+                    "CHANNEL_NAME",
+                    NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(mChannel)
+        }
+
+        // 알림 생성
+        val notification: Notification = Notification.Builder(this, "CHANNEL_ID")
+            .setSmallIcon(R.drawable.ic_play)
+            .setContentTitle("뮤직 플레이어 앱")
+            .setContentText("앱이 실행 중입니다.")
+            .build()
+        // 서비스를 포그라운드 서비스로 만듬, startForeground(알림의 식별자, 보여줄 알림)
+        startForeground(1, notification)
     }
 
-    fun startForegroundService() {} // 알림 채널 생성, API 26부터는 서비스 실행됨을 알림
-    fun isPlaying() {}  // 재생 중인지 확인
+    // 서비스 생명 주기의 마지막 단계, onCreate()에서 상태표시줄에 보여주었던 알림 해제
+    // 서비스 중단 처리
+    override fun onDestroy() {
+        super.onDestroy()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            stopForeground(true);   // 포그라운드 서비스를 멈춤
+        }
+    }
+
+    // 재생 중인지 확인
+    fun isPlaying() : Boolean{
+        return (mMediaPlayer != null && mMediaPlayer?.isPlaying ?: false)
+    }
+
     fun play() {}       // 재생
     fun pause() {}      // 일시정지
     fun stop() {}       // 완전 정지
