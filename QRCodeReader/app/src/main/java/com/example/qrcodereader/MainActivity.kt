60eafd48a35083c228c41f542a87b41f15ba55af
DESKTOP-GNOW\gnow <gnow@chungbuk.ac.kr>
2023-02-09 17:43:38 +0900

    CameraPermission

--- QRCodeReader/app/src/main/java/com/example/qrcodereader/MainActivity.kt
+++ QRCodeReader/app/src/main/java/com/example/qrcodereader/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.qrcodereader
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
@@ -15,6 +19,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var cameraProviderFuture:
             ListenableFuture<ProcessCameraProvider>
     // ListenableFuture 에 태스트가 제대로 끝났을 때 동작 지정
+    
+    private val PERMISSIONS_REQUEST_CODE = 1    // 태그 기능 코드, 0과 같거나 큰 양수면 상관 없음
+    private val PERMISSIONS_REQUIRED = arrayOf(Manifest.permission.CAMERA)  // 카메라 권한 지정
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,9 +31,39 @@ class MainActivity : AppCompatActivity() {
         // binding.root → 바인딩 클래스라면 항상 자동으로 생성되는 루트 뷰를 반환
         val view = binding.root
         setContentView(view)
-        
-        startCamera()   // 카메라 시작(onCreate() 안에 넣는다)
 
+        if (!hasPermissions(this)) {
+            // 카메라 권한을 요청
+            requestPermissions(PERMISSIONS_REQUIRED, PERMISSIONS_REQUEST_CODE)
+        } else {
+            // 만약 이미 권한이 있다면 카메라를 시작
+            startCamera()   // 카메라 시작(onCreate() 안에 넣는다)
+        }
+    }
+
+    // 권한 유무 확인
+    fun hasPermissions(context: Context) = PERMISSIONS_REQUIRED.all {
+        ContextCompat.checkSelfPermission(context, it) ==
+                PackageManager.PERMISSION_GRANTED
+    }
+
+    // 권한 요청 콜백 함수
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        // onCreate 의 requestPermissions  인수와 맞는지 확인
+        if (requestCode == PERMISSIONS_REQUEST_CODE) {
+            if (PackageManager.PERMISSION_GRANTED == grantResults.firstOrNull()) {
+                Toast.makeText(this@MainActivity, "권한 요청이 승인되었습니다.",
+                Toast.LENGTH_LONG).show()
+                startCamera()
+            } else {
+                Toast.makeText(this@MainActivity, "권한 요청이 거부되었습니다.",
+                Toast.LENGTH_LONG).show()
+                finish()
+            }
+        }
     }
 
     // 미리보기와 이미지 분석 시작
