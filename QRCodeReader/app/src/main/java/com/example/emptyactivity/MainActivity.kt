023153a7b437585af485710c0c430f30e5042252
DESKTOP-GNOW\gnow <gnow@chungbuk.ac.kr>
2023-02-09 16:28:32 +0900

    Preview_startCamera()

--- QRCodeReader/app/src/main/java/com/example/emptyactivity/MainActivity.kt
+++ QRCodeReader/app/src/main/java/com/example/emptyactivity/MainActivity.kt
@@ -2,12 +2,19 @@ package com.example.emptyactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.camera.core.CameraSelector
 import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import com.example.emptyactivity.databinding.ActivityMainBinding
+import com.google.common.util.concurrent.ListenableFuture
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding  // 바인딩 변수 선언
+    private lateinit var cameraProviderFuture:
+            ListenableFuture<ProcessCameraProvider>
+    // ListenableFuture 에 태스트가 제대로 끝났을 때 동작 지정
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,12 +24,26 @@ class MainActivity : AppCompatActivity() {
         // binding.root → 바인딩 클래스라면 항상 자동으로 생성되는 루트 뷰를 반환
         val view = binding.root
         setContentView(view)
+        
+        startCamera()   // 카메라 시작(onCreate() 안에 넣는다)
 
     }
 
     // 미리보기와 이미지 분석 시작
     fun startCamera() {
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this)  // 객체의 참조값 할당
+        cameraProviderFuture.addListener(Runnable { // 태스크가 끝나면 실행
+            // 카메라 생명 주기를 액티비티나 프래그먼트와 같은 생명 주기에 바인드해줌
+            val cameraProvider = cameraProviderFuture.get()
 
+            val preview = getPreview()  // 미리보기 객체 가져오기
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+            // 후면 카메라 선택(DEFAULT_BACK_CAMERA)
+
+            cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+            // 미리보기 기능 선택(preview)
+
+        }, ContextCompat.getMainExecutor(this))
     }
 
     // 미리보기 객체 반환
