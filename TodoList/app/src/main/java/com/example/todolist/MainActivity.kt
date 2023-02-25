6b1e5b58f25b05a5915e311f384bcf82a1ebb9a8
DESKTOP-GNOW\gnow <gnow@chungbuk.ac.kr>
2023-02-25 23:39:07 +0900

    Road Todo Data

--- TodoList/app/src/main/java/com/example/todolist/MainActivity.kt
+++ TodoList/app/src/main/java/com/example/todolist/MainActivity.kt
@@ -4,10 +4,18 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.todolist.databinding.ActivityMainBinding
+import com.example.todolist.db.AppDatabase
+import com.example.todolist.db.ToDoDao
+import com.example.todolist.db.ToDoEntity
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    
+    private lateinit var db : AppDatabase
+    private lateinit var todoDao : ToDoDao
+    private lateinit var todoList : ArrayList<ToDoEntity>
+    
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,5 +26,22 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, AddTodoActivity::class.java)
             startActivity(intent)
         }
+        
+        // DB 인스턴스를 가져오고 DB 작업 을 할 수 있는 DAO 를 가져옵니다.
+        db = AppDatabase.getInstance(this)!!
+        todoDao = db.getTodoDao()
+        
+        getAllTodoList()    /// 할 일 리스트 가져오기
+    }
+    
+    private fun getAllTodoList() {
+        Thread {
+            todoList = ArrayList(todoDao.getAll())
+            setRecyclerView()
+        }.start()
+    }
+    
+    private fun setRecyclerView() {
+        // 리사이클러뷰 설정
     }
 }
