3fb1aad7dfda5032ef763f80e64b874404a74af5
DESKTOP-GNOW\gnow <gnow@chungbuk.ac.kr>
2023-02-27 23:02:53 +0900

    Basic Structure of RecyclerView Adapter

---
+++ TodoList/app/src/main/java/com/example/todolist/TodoRecycleViewAdapter.kt
@@ -0,0 +1,31 @@
+package com.example.todolist
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.todolist.databinding.ItemTodoBinding
+import com.example.todolist.db.ToDoEntity
+
+class TodoRecycleViewAdapter(private val todoList : ArrayList<ToDoEntity>)
+    : RecyclerView.Adapter<TodoRecycleViewAdapter.MyViewHolder>() {
+
+        inner class MyViewHolder(binding : ItemTodoBinding) :
+            RecyclerView.ViewHolder(binding.root) {
+                val tv_importance = binding.tvImportance
+                val tv_title = binding.tvTitle
+
+                // 뷰 바인딩에서 기본적으로 제공하는 root 변수는 레이아웃의 루트 레이아웃을 의미
+                val root = binding.root
+            }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+    
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
